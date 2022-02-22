2f84447f2509cc50cfebdd684b4531330ea4e846
twinoxen <1854037+twinoxen@users.noreply.github.com>
2022-02-22 15:32:40 -0500

    finish solana contract

--- programs/anchor-crunchy-vs-smooth/src/lib.rs
+++ programs/anchor-crunchy-vs-smooth/src/lib.rs
@@ -5,10 +5,50 @@ declare_id!("JBtS6J2Z8R6TbXoiDyTpThQK4eKa3bdg77dHvfWuHRox");
 #[program]
 pub mod anchor_crunchy_vs_smooth {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, vote_account_bump: u8) -> ProgramResult {
+        ctx.accounts.vote_account.bump = vote_account_bump;
+        Ok(())
+    }
+
+    pub fn vote_crunchy(ctx: Context<Vote>) -> ProgramResult {
+        ctx.accounts.vote_account.crunchy +=1;
+        Ok(())
+    }
+
+    pub fn vote_smooth(ctx: Context<Vote>) -> ProgramResult {
+        ctx.accounts.vote_account.smooth +=1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(vote_account_bump: u8)]
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        seeds = [b"vote_account".as_ref()],
+        bump = vote_account_bump,
+        payer = user
+    )]
+    vote_account: Account<'info, VotingState>,
+    user: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Vote<'info> {
+    #[account(
+        mut, 
+        seeds = [b"vote_account".as_ref()],
+        bump = vote_account.bump,
+    )]
+    vote_account: Account<'info, VotingState>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct VotingState {
+    crunchy: u64,
+    smooth: u64,
+    bump: u8,
+}
--- tests/anchor-crunchy-vs-smooth.ts
+++ tests/anchor-crunchy-vs-smooth.ts
@@ -1,17 +1,93 @@
 import * as anchor from '@project-serum/anchor';
+import assert from 'assert';
 import { Program } from '@project-serum/anchor';
 import { AnchorCrunchyVsSmooth } from '../target/types/anchor_crunchy_vs_smooth';
 
+const { web3, BN } = anchor;
+const { SystemProgram, } = web3;
+
 describe('anchor-crunchy-vs-smooth', () => {
-
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.AnchorCrunchyVsSmooth as Program<AnchorCrunchyVsSmooth>;
+  const program = anchor.workspace
+    .AnchorCrunchyVsSmooth as Program<AnchorCrunchyVsSmooth>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  it('Initializes with 0 votes for crunchy and smooth', async () => {
+    const [voteAccount, accountBump] =
+      await anchor.web3.PublicKey.findProgramAddress(
+        [Buffer.from('vote_account')],
+        program.programId
+      );
+
+    console.log('Testing Initialize...');
+    // The last element passed to RPC methods is always the transaction options
+    // Because voteAccount is being created here, we are required to pass it as a signers array
+    await program.rpc.initialize(new BN(accountBump) as unknown as number, {
+      accounts: {
+        voteAccount: voteAccount,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      }
+    });
+
+    const account = await program.account.votingState.fetch(
+      voteAccount
+    );
+    console.log('Crunchy: ', account.crunchy.toString());
+    console.log('Smooth: ', account.smooth.toString());
+    assert.ok(
+      parseInt(account.crunchy.toString()) == 0 && parseInt(account.smooth.toString()) == 0
+    );
+  });
+  it('Votes correctly for crunchy', async () => {
+    const [voteAccount] =
+      await anchor.web3.PublicKey.findProgramAddress(
+        [Buffer.from('vote_account')],
+        program.programId
+      );
+
+    console.log('Testing voteCrunchy...');
+    await program.rpc.voteCrunchy({
+      accounts: {
+        voteAccount: voteAccount,
+      },
+    });
+
+    const account = await program.account.votingState.fetch(
+      voteAccount
+    );
+    console.log('Crunchy: ', account.crunchy.toString());
+    console.log('Smooth: ', account.smooth.toString());
+
+    assert.ok(
+      parseInt(account.crunchy.toString()) == 1 && parseInt(account.smooth.toString()) == 0
+    );
+  });
+  it('Votes correctly for smooth', async () => {
+    console.log('Testing voteSmooth...');
+
+    const [voteAccount] =
+      await anchor.web3.PublicKey.findProgramAddress(
+        [Buffer.from('vote_account')],
+        program.programId
+      );
+
+    await program.rpc.voteSmooth({
+      accounts: {
+        voteAccount: voteAccount,
+      },
+    });
+
+    const account = await program.account.votingState.fetch(
+      voteAccount
+    );
+    console.log('Crunchy: ', account.crunchy.toString());
+    console.log('Smooth: ', account.smooth.toString());
+
+    assert.ok(
+      parseInt(account.crunchy.toString()) == 1 && parseInt(account.smooth.toString()) == 1
+    );
   });
 });
